a5732a71429aeff59345fd918936c613c1dc34f8
Moorviper <moorviper@magdlos.com>
2015-12-08 17:37:44 +0100

    init README.md

---
+++ orga/create_readme.sh
@@ -0,0 +1,239 @@
+
+cd buffalo-wzr-hp-ag300h-wzr-600dhp
+touch README.md 
+cd..  
+cd buffalo-wzr-hp-g450h
+touch README.md 
+cd..  
+cd d-link-dir-615-rev-c1
+touch README.md 
+cd..  
+cd d-link-dir-825-rev-b1
+touch README.md 
+cd..  
+cd gl-inet-6408a-v1
+touch README.md 
+cd..  
+cd gl-inet-6416a-v1
+touch README.md 
+cd..  
+cd linksys-wrt160nl
+touch README.md 
+cd..  
+cd netgear-wndr3700
+touch README.md 
+cd..  
+cd netgear-wndr3700v2
+touch README.md 
+cd..  
+cd netgear-wndr3700v4
+touch README.md 
+cd..  
+cd netgear-wndr3800
+touch README.md 
+cd..  
+cd netgear-wndr4300
+touch README.md 
+cd..  
+cd netgear-wndrmacv2
+touch README.md 
+cd..  
+cd tp-link-cpe210-v1.0
+touch README.md 
+cd..  
+cd tp-link-cpe220-v1.0 
+touch README.md 
+cd..  
+cd tp-link-cpe510-v1.0
+touch README.md 
+cd..  
+cd tp-link-cpe520-v1.0
+touch README.md 
+cd..  
+cd tp-link-tl-mr3020-v1
+touch README.md 
+cd..  
+cd tp-link-tl-mr3040-v1
+touch README.md 
+cd..  
+cd tp-link-tl-mr3040-v2
+touch README.md 
+cd..  
+cd tp-link-tl-mr3220-v1
+touch README.md 
+cd..  
+cd tp-link-tl-mr3220-v2
+touch README.md 
+cd..  
+cd tp-link-tl-mr3420-v1
+touch README.md 
+cd..  
+cd tp-link-tl-mr3420-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wa701n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa701n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wa750re-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa801n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa801n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wa830re-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa830re-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wa850re-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa860re-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wa901n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wa901n-nd-v3
+touch README.md 
+cd..  
+cd tp-link-tl-wdr3500-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wdr3600-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wdr4300-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wdr4900-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr703n-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr710n-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr740n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr740n-nd-v3
+touch README.md 
+cd..  
+cd tp-link-tl-wr740n-nd-v4
+touch README.md 
+cd..  
+cd tp-link-tl-wr740n-nd-v5
+touch README.md 
+cd..  
+cd tp-link-tl-wr741n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr741n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wr741n-nd-v4
+touch README.md 
+cd..  
+cd tp-link-tl-wr741n-nd-v5
+touch README.md 
+cd..  
+cd tp-link-tl-wr743n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr743n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wr841n-nd-v3
+touch README.md 
+cd..  
+cd tp-link-tl-wr841n-nd-v5
+touch README.md 
+cd..  
+cd tp-link-tl-wr841n-nd-v7
+touch README.md 
+cd..  
+cd tp-link-tl-wr841n-nd-v8
+touch README.md 
+cd..  
+cd tp-link-tl-wr841n-nd-v9
+touch README.md 
+cd..  
+cd tp-link-tl-wr842n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr842n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wr941n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wr941n-nd-v3
+touch README.md 
+cd..  
+cd tp-link-tl-wr941n-nd-v4
+touch README.md 
+cd..  
+cd tp-link-tl-wr941n-nd-v5
+touch README.md 
+cd..  
+cd tp-link-tl-wr1043n-nd-v1
+touch README.md 
+cd..  
+cd tp-link-tl-wr1043n-nd-v2
+touch README.md 
+cd..  
+cd tp-link-tl-wr2543n-nd-v1
+touch README.md 
+cd..  
+cd ubiquiti-bullet-m
+touch README.md 
+cd..  
+cd ubiquiti-loco-m-xw
+touch README.md 
+cd..  
+cd ubiquiti-loco-m
+touch README.md 
+cd..  
+cd ubiquiti-nanostation-m-xw
+touch README.md 
+cd..  
+cd ubiquiti-nanostation-m
+touch README.md 
+cd..  
+cd ubiquiti-picostation-m
+touch README.md 
+cd..  
+cd ubiquiti-rocket-m
+touch README.md 
+cd..  
+cd ubiquiti-unifi-ap-pro
+touch README.md 
+cd..  
+cd ubiquiti-unifi
+touch README.md 
+cd..  
+cd ubiquiti-unifiap-outdoor
+touch README.md 
+cd..  
+cd x86-generic.img
+touch README.md 
+cd..  
+cd x86-kvm.img
+touch README.md 
+cd..  
+cd x86-virtualbox.vdi
+touch README.md 
+cd..  
+cd x86-vmware.vmdk
+touch README.md 
+cd..  
+
