b6b9158a2a69796d22c2d5dd58850d753f2d612d
Moorviper <moorviper@magdlos.com>
2016-05-16 01:33:58 +0200

    Update screate wiki.sh gluon 2016.1.x

--- orga/create-wiki.sh
+++ orga/create-wiki.sh
@@ -114,6 +114,67 @@ x86-vmware.vmdk"
 ROUTERSxiaomi="\
 	xiaomi-miwifi-mini"
 
+ROUTERS_2016_1="\
+buffalo-wzr-hp-g300nh \
+d-link-dir-505-rev-a1 \
+tp-link-cpe210-v1.1 \
+tp-link-cpe220-v1.1 \
+tp-link-cpe510-v1.1 \
+tp-link-cpe520-v1.1 \
+tp-link-tl-wa901n-nd-v1 \
+tp-link-tl-wr710n-v2 \
+tp-link-tl-wr710n-v2.1 \
+tp-link-tl-wr801n-nd-v1 \
+tp-link-tl-wr801n-nd-v2 \
+tp-link-tl-wr841n-nd-v10 \
+tp-link-tl-wr843n-nd-v1 \
+tp-link-tl-wr940n-v1 \
+tp-link-tl-wr940n-v2 \
+tp-link-tl-wr940n-v3 \
+tp-link-tl-wr941n-nd-v6 \
+tp-link-tl-wr1043n-nd-v3 \
+ubiquiti-airgateway \
+ubiquiti-airrouter \
+ubiquiti-bullet-m \
+ubiquiti-bullet-m2 \
+ubiquiti-bullet-m5 \
+ubiquiti-loco-m-xw \
+ubiquiti-ls-sr71 \
+ubiquiti-nanostation-loco-m2 \
+ubiquiti-nanostation-loco-m5 \
+ubiquiti-nanostation-m \
+ubiquiti-nanostation-m-xw \
+ubiquiti-nanostation-m2 \
+ubiquiti-nanostation-m5 \
+ubiquiti-picostation-m2 \
+ubiquiti-rocket-m-xw \
+ubiquiti-rocket-m2 \
+ubiquiti-rocket-m5 \
+ubiquiti-unifi-ap-pro \
+ubiquiti-unifi \
+ubiquiti-unifiap-outdoor+ \
+ubiquiti-unifiap-outdoor \
+wd-my-net-n600 \
+wd-my-net-n750"
+
+
+ROUTERS_2016_1_1="\
+onion-omega \
+tp-link-tl-mr13u-v1"
+
+ROUTERS_2016_1_3="\
+alfa-ap121 \
+alfa-ap121u \
+alfa-hornet-ub \
+tp-link-tl-wa7510n-v1"
+
+
+ROUTERS_2016_1_4="\
+8devices-carambola2-board \
+meraki-mr12 \
+meraki-mr16 \
+meraki-mr62 \
+meraki-mr66"
 
 rm wiki.txt
 touch wiki.txt
@@ -232,6 +293,8 @@ do
   # mkdir $routers ;
 done
 
+	echo "### Gluon 2016.x dive" >> wiki.txt
+	echo "***" >> wiki.txt
 
 	echo "### Xiaomi" >> wiki.txt
 	echo "***" >> wiki.txt
@@ -242,8 +305,82 @@ do
 	echo "$routers |<img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/front.jpg" height="200" width="222">    | <img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/back.jpg" height="200" width="222">   |" >> wiki.txt;  
  	echo "|   |   |   |   |   |" >> wiki.txt;
   	echo "$routers ";
+done
+
+  	echo "### Gluon 2016.1 added Device" >> wiki.txt
+	echo "***" >> wiki.txt
+
+
+
+  	echo "### ar71xx-generic" >> wiki.txt
+	echo "***" >> wiki.txt
+	echo "| Router Name  	| front | back |" >> wiki.txt;
+	echo "|---	        |---	|---   |" >> wiki.txt;
+for routers in `echo $ROUTERS_2016_1` 
+do
+	echo "$routers |<img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/front.jpg" height="200" width="222">    | <img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/back.jpg" height="200" width="222">   |" >> wiki.txt;  
+ 	echo "|   |   |   |   |   |" >> wiki.txt;
+  	echo "$routers ";
+  # mkdir $routers ;
+done
+
+################################################################################################################################################
+
+  	echo "### Gluon 2016.1.1 added Device" >> wiki.txt
+	echo "***" >> wiki.txt
+
+
+
+  	echo "### ar71xx-generic" >> wiki.txt
+	echo "***" >> wiki.txt
+	echo "| Router Name  	| front | back |" >> wiki.txt;
+	echo "|---	        |---	|---   |" >> wiki.txt;
+for routers in `echo $ROUTERS_2016_1_1` 
+do
+	echo "$routers |<img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/front.jpg" height="200" width="222">    | <img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/back.jpg" height="200" width="222">   |" >> wiki.txt;  
+ 	echo "|   |   |   |   |   |" >> wiki.txt;
+  	echo "$routers ";
+  # mkdir $routers ;
+done
+
+################################################################################################################################################
+
+  	echo "### Gluon 2016.1.3 added Device" >> wiki.txt
+	echo "***" >> wiki.txt
+
+
+
+  	echo "### ar71xx-generic" >> wiki.txt
+	echo "***" >> wiki.txt
+	echo "| Router Name  	| front | back |" >> wiki.txt;
+	echo "|---	        |---	|---   |" >> wiki.txt;
+for routers in `echo $ROUTERS_2016_1_3` 
+do
+	echo "$routers |<img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/front.jpg" height="200" width="222">    | <img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/back.jpg" height="200" width="222">   |" >> wiki.txt;  
+ 	echo "|   |   |   |   |   |" >> wiki.txt;
+  	echo "$routers ";
+  # mkdir $routers ;
+done
+
+################################################################################################################################################
+
+  	echo "### Gluon 2016.1.4 added Device" >> wiki.txt
+	echo "***" >> wiki.txt
+
+
+
+  	echo "### ar71xx-generic" >> wiki.txt
+	echo "***" >> wiki.txt
+	echo "| Router Name  	| front | back |" >> wiki.txt;
+	echo "|---	        |---	|---   |" >> wiki.txt;
+for routers in `echo $ROUTERS_2016_1_4` 
+do
+	echo "$routers |<img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/front.jpg" height="200" width="222">    | <img src="https://raw.githubusercontent.com/Moorviper/Freifunk-Router-Anleitungen/master/$routers/back.jpg" height="200" width="222">   |" >> wiki.txt;  
+ 	echo "|   |   |   |   |   |" >> wiki.txt;
+  	echo "$routers ";
   # mkdir $routers ;
 done
 
 
 
+ 
