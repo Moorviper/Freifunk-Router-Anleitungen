bb40c290e7261626616fca554505955197723110
Daniel Krah <moorviper@gmail.com>
2018-03-06 19:28:13 +0100

    [update] latexdockercmd, build.sh
    try to silence luatex build

--- latexdockercmd.sh
+++ latexdockercmd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 IMAGE=moorviper/dockerlatex
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data "$IMAGE" "$@"  1>/dev/null 2>&1
